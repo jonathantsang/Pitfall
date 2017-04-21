2aef5262305c079e9e3ad637a16626f4b63917c7
jonathantsang <j26tsang@gmail.com>
2017-04-21 00:13:55 -0400

    basic grid, player

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : MonoBehaviour {
+
+	public static GameData instance;
+	public GameObject player;
+
+	public levelData[] LevelData;
+
+	void Awake(){
+		// Singleton Behaviour
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy(gameObject);    
+		DontDestroyOnLoad(gameObject);
+	}
+
+	[System.Serializable]
+	public class levelData{
+		public int startPointX;
+		public int startPointY;
+		public int endPointX;
+		public int endPointY;
+	}
+		
+
+	// Use this for initialization
+	void Start () {
+		Instantiate (player, new Vector2 (LevelData [0].startPointX, LevelData [0].startPointY), Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/spawnGrid.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnGrid : MonoBehaviour {
+
+	public GameObject tile;
+
+	private boundary bounds;
+	private Vector2 curPos;
+	private Transform boardHolder;
+	public List<Vector3> gridPositions = new List<Vector3> ();
+
+	private int wid = 5;
+	private int hei = 5;
+
+	[System.Serializable]
+	public class boundary{
+		public int width;
+		public int height;
+
+		public boundary(int w, int h){
+			width = w;
+			height = h;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		bounds = new boundary (wid, hei);
+		curPos = new Vector2(-2, -2);
+		generateGrid ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void InitialiseList()
+	{
+		gridPositions.Clear ();
+
+		for (int x = 0; x < bounds.height; x++) 
+		{
+			for (int y = 1; y < bounds.width; y++) 
+			{
+				gridPositions.Add (new Vector3 (x, y, 0f));
+			}
+		}
+	}
+
+	void generateGrid(){
+		boardHolder = new GameObject ("Board").transform;
+		for (int i = 0; i < bounds.height; i++) {
+			for (int j = 0; j < bounds.width; j++) {
+				GameObject instance = Instantiate (tile, new Vector2(curPos.x + j, curPos.y + i), Quaternion.identity);
+				instance.transform.SetParent (boardHolder);
+				gridPositions.Add (new Vector3 (i, j, 0f));
+			}
+		}
+	}
+}
