f5866c81074e6b1cee0f34737a2c8b9e0193b6f4
jonathantsang <j26tsang@gmail.com>
2017-04-26 00:16:21 -0400

    enemy elements

---
+++ Assets/Scripts/FireColumnBeam.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireColumnBeam : MonoBehaviour {
+
+	private GameObject beam;
+
+	// Use this for initialization
+	void Start () {
+		// Beam is second child
+		beam = gameObject.transform.GetChild (1).gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
