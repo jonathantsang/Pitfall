ea0bb55cd9dabd8d0c57535ca4bd5dfbbcd8ca92
jonathantsang <j26tsang@gmail.com>
2017-04-29 17:36:25 -0400

    number backend done

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MovingObject {
+
+	public static Player instance;
+
+	// Use this for initialization
+	void Start () {
+		base.Start ();
+
+		// Singleton Behaviour
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy(gameObject);    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movement ();
+	}
+
+
+}
